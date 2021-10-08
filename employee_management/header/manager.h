d32aa7b176b64c7bd05ad26f3a68c45be5d1c129
coolsyn2000 <337113250@qq.com>
2021-10-08 13:43:38 +0800

    project

--- employee_management/header/manager.h
+++ employee_management/header/manager.h
@@ -12,7 +12,9 @@ using namespace std;
 #include "worker.h"
 class Manager:public Worker{
 public:
+
     Manager(int id, string name,int dId);
     virtual void showInfo();
     virtual string getDeptName();
+
 };
